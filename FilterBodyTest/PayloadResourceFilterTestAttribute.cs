7bf17caa33d9325634fe04195d67bf30a6ac1807
Markus Fehn <markus.fehn@gmx.net>
2021-04-29 20:01:58 +0200

    Refactored filter attribute to be stateless and thread-safe duue to @Tratcher's find https://github.com/dotnet/aspnetcore/issues/32241#issuecomment-829369175

--- FilterBodyTest/PayloadResourceFilterTestAttribute.cs
+++ FilterBodyTest/PayloadResourceFilterTestAttribute.cs
@@ -35,9 +35,9 @@ namespace mfsd.Core.Mvc.Filters
         /// </summary>
         public int JsonBufferSize { get; set; } = 4096;
 
-        private ResourceExecutingContext _filterContext;
-        private HttpRequest _request;
-        private string _url;
+        //private ResourceExecutingContext _filterContext;
+        //private HttpRequest _request;
+        //private string _url;
 
 
         public PayloadResourceFilterTestAttribute()
@@ -47,16 +47,13 @@ namespace mfsd.Core.Mvc.Filters
 
         public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
         {
-            _filterContext = context;
-            _filterContext.HttpContext.Items["RequestPayload"] = _requestPayload;
+            var filterContext = context;
+            filterContext.HttpContext.Items["RequestPayload"] = _requestPayload;
 
-            _request = _filterContext.HttpContext.Request;
-            _url = UriHelper.GetDisplayUrl(_request) + (_request.QueryString.HasValue ? _request.QueryString.Value : "");
-
-            var proceed = await getJsonRequestData();
+            var proceed = await getJsonRequestData(filterContext);
             if (!proceed) return;
 
-            getOtherRequestData();
+            getOtherRequestData(filterContext);
             await next();
         }
 
@@ -67,64 +64,67 @@ namespace mfsd.Core.Mvc.Filters
         // rx to test for "double" JSON
         private static Regex _rxJsonTwice = new Regex(@"}\s*{");
 
-        public async Task<bool> getJsonRequestData()
+        public async Task<bool> getJsonRequestData(ResourceExecutingContext filterContext)
         {
-            if (_request.ContentType == null ||
-                !_request.ContentType.StartsWith("application/json", StringComparison.InvariantCultureIgnoreCase))
+            var request = filterContext.HttpContext.Request;
+
+            if (request.ContentType == null ||
+                !request.ContentType.StartsWith("application/json", StringComparison.InvariantCultureIgnoreCase))
             {
                 return true;
             }
 
 
-            if (!_request.Body.CanSeek)
+            if (!request.Body.CanSeek)
             {
-                _request.EnableBuffering();
+                request.EnableBuffering();
             }
-            _request.Body.Position = 0;
+            request.Body.Position = 0;
 
             using var reader = new StreamReader(
-                    _request.Body,
+                    request.Body,
                     encoding: Encoding.UTF8,
                     detectEncodingFromByteOrderMarks: false,
                     leaveOpen: true);
 
             var body = (await reader.ReadToEndAsync()) ?? "";
-            _request.Body.Position = 0;
+            request.Body.Position = 0;
 
-            _filterContext.HttpContext.Items["BodyPayload"] = body;
-            _filterContext.HttpContext.Items["Message"] = "OK";
+            filterContext.HttpContext.Items["BodyPayload"] = body;
+            filterContext.HttpContext.Items["Message"] = "OK";
 
             if (body == "")
             {
-                _filterContext.HttpContext.Items["Message"] = "Empty JSON request";
+                filterContext.HttpContext.Items["Message"] = "Empty JSON request";
             }
             else
             {
                 if(!_rxJsonStart.IsMatch(body) || !_rxJsonEnd.IsMatch(body))
                 {
-                    _filterContext.HttpContext.Items["Message"] = "JSON syntax error";
+                    filterContext.HttpContext.Items["Message"] = "JSON syntax error";
                 }
                 else if(_rxJsonTwice.IsMatch(body))
                 {
-                    _filterContext.HttpContext.Items["Message"] = "Double JSON";
+                    filterContext.HttpContext.Items["Message"] = "Double JSON";
                 }
             }
 
             return true;
         }
 
-        public void getOtherRequestData()
+        public void getOtherRequestData(ResourceExecutingContext filterContext)
         {
+            var request = filterContext.HttpContext.Request;
 
-            foreach (var kv in _request.Query)
+            foreach (var kv in request.Query)
             {
                 _requestPayload[kv.Key] = kv.Value.Last();
                 _hasQueryStringPayload = true;
             }
 
-            if (_request.HasFormContentType && _request.Form != null && _request.Form.Count() > 0)
+            if (request.HasFormContentType && request.Form != null && request.Form.Count() > 0)
             {
-                foreach (var kv in _request.Form)
+                foreach (var kv in request.Form)
                 {
                     _requestPayload[kv.Key] = kv.Value.Last();
                     _hasFormPayload = true;
